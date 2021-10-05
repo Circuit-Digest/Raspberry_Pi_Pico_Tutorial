49d4bf850dde9a212e16be8e118b17f05169fa69
joydipdutta001 <53464193+joydipdutta001@users.noreply.github.com>
2021-10-05 12:03:28 +0530

    added T9

---
+++ T9_DualCore_Program_PIco/Codes/main.py
@@ -0,0 +1,39 @@
+import machine
+import utime
+import _thread
+
+led1 = machine.Pin(16, machine.Pin.OUT)
+led2 = machine.Pin(15, machine.Pin.OUT)
+
+
+sLock = _thread.allocate_lock()
+
+def CoreTask():
+    while True:
+        sLock.acquire()
+        print("Entered into the second Thred")
+        utime.sleep(1)
+        led2.high()
+        print("Led 2 turned on")
+        utime.sleep(2)
+        led2.low()
+        print("Led 2 turned off")
+        utime.sleep(1)
+        print("Exiting from the 2nd Thread")
+        utime.sleep(1)
+        sLock.release()
+
+_thread.start_new_thread(CoreTask, ())
+
+while True:
+    # We acquire the semaphore lock
+    sLock.acquire()
+    print("Entered into the main Thred")
+    led1.toggle()
+    utime.sleep(0.15)
+    print("Led 1 started to toggle.")
+    utime.sleep(1)
+    print("Exiting from the main Thread")
+    utime.sleep(1)
+    # We release the semaphore lock
+    sLock.release()
---
+++ T9_DualCore_Program_PIco/README.md
@@ -0,0 +1,8 @@
+# Dual core programming on the Raspberry Pi Pico using MicroPython
+
+
+
+<br>
+<br>
+Coming Soon,....
+<br>
